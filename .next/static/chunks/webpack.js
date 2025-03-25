35d94c0dd48d29d72a07b2f3933ad8adbee01bdb
pushrefresh <rossi@pushrefresh.com>
2025-03-25 15:21:09 -0500

    fixing min-width on complete button

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -162,7 +162,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b568222037be5f69"; }
+/******/ 		__webpack_require__.h = function() { return "29c0f9e98bb386e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
